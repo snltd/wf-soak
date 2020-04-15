ef6075e5b8ab0005598d086c976d0e1da6aa5cae
Robert Fisher <services@id264.net>
2020-04-16 00:16:52 +0100

    1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wf-soaker "1.1.0"
+(defproject wf-soaker "1.2.0"
   :description "Send a steady point rate to Wavefront"
   :url "https://github.com/snltd/wf-soak"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
